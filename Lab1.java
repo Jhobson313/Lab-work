a4c561fb10e1c95ba58ca653cb658e8615ceb691
Jph <Jph>
2016-08-25 13:35:03 -0400

    iniatil commit

---
+++ Lab1.java
@@ -0,0 +1,36 @@
+
+
+import java.util.Scanner;
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in); 
+	System.out.println("Welcome to Grand Circus' Room Detail Generator!");
+	
+	System.out.println("Enter Length");
+	double length = scanner.nextDouble();
+			
+	System.out.println("Enter Width");
+	double width = scanner.nextDouble();
+	
+	System.out.println("Area");
+	double area = length * width ;
+	System.out.println(area);
+	
+	
+	System.out.println("Perimeter");
+	double perimeter = 2 * area;
+	System.out.println(perimeter);
+	
+	
+
+	}
+}
+	
+			
+		
+	
+	
+	
